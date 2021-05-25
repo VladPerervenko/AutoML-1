674a9f122f5064b3f074d722b9ac5952e66c7ac4
Niels Uitterdijk <niels.uit@gmail.com>
2021-05-25 13:29:54 +0200

    Verbose, asserts, best

--- src/Amplo/GridSearch/BaseGridSearch.py
+++ src/Amplo/GridSearch/BaseGridSearch.py
@@ -1,17 +1,17 @@
 import time
 import copy
+import datetime
 import itertools
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from sklearn import metrics
-from sklearn.model_selection import KFold
-from sklearn.model_selection import StratifiedKFold
 
 
 class BaseGridSearch:
 
-    def __init__(self, model, params, cv=None, scoring=metrics.SCORERS['accuracy']):
+    def __init__(self, model, params, cv=None, scoring=metrics.SCORERS['accuracy'],
+                 verbose=0):
         """
         Basic exhaustive grid search.
         @param model: Model object to optimize
@@ -24,17 +24,20 @@ class BaseGridSearch:
         self.params = params
         self.cv = cv
         self.scoring = scoring
+        self.verbose = verbose
 
         # Checks
         assert model is not None, 'Model not provided'
         assert isinstance(params, dict), 'Parameter set should be dictionary'
-        assert all([isinstance(x, list) for x in params.values()]), 'Parameter set dictionairy needs to be filled ' \
+        assert all([isinstance(x, list) for x in params.values()]), 'Parameter set dictionary needs to be filled ' \
                                                                     'with lists '
+        assert scoring in metrics.SCORERS.values(), 'Chose a scorer from sklearn.metrics.SCORERS'
         
         # Initiate            
         self.parsed_params = []
         self.result = []
         self._parse_params()
+        self.best = [None, None]    # Mean score, Std score
 
     def _parse_params(self):
         k, v = zip(*self.params.items())
@@ -46,9 +49,9 @@ class BaseGridSearch:
 
     def fit(self, x, y):
         # Convert to Numpy
-        if isinstance(x, pd.DataFrame) or isinstance(x, pd.core.series.Series):
+        if isinstance(x, pd.DataFrame) or isinstance(x, pd.Series):
             x = np.array(x)
-        if isinstance(y, pd.DataFrame) or isinstance(y, pd.core.series.Series):
+        if isinstance(y, pd.DataFrame) or isinstance(y, pd.Series):
             y = np.array(y).reshape((-1))
 
         # Loop through parameters
@@ -61,16 +64,16 @@ class BaseGridSearch:
                 t = time.time()
 
                 # Split data
-                xtrain, xval = x[train_ind], x[val_ind]
-                ytrain, yval = y[train_ind], y[val_ind]
+                x_train, x_val = x[train_ind], x[val_ind]
+                y_train, y_val = y[train_ind], y[val_ind]
 
                 # Model training
                 model = copy.copy(self.model)
                 model.set_params(**param)
-                model.fit(xtrain, ytrain)
+                model.fit(x_train, y_train)
 
                 # Results
-                scoring.append(self.scoring(model.predict(xval), yval))
+                scoring.append(self.scoring(model, x_val, y_val))
                 timing.append(time.time() - t)
 
             # Compare scores
@@ -81,8 +84,10 @@ class BaseGridSearch:
                 if np.mean(scoring) - np.std(scoring) > self.best[0] - self.best[1]:
                     self.best = [np.mean(scoring), np.std(scoring)]
 
-            # print('[GridSearch] [AutoML] Score: %.4f \u00B1 %.4f (in %.1f seconds) (Best score so-far: %.4f \u00B1 %.4f) (%i / %i)' %
-            #       (datetime.now().strftime('%H:%M'), np.mean(scoring), np.std(scoring), np.mean(timing), self.best[0], self.best[1], i + 1, len(self.parsed_params)))
+            if self.verbose > 0:
+                print('[GridSearch][%s] Score: %.4f \u00B1 %.4f (in %.1f seconds) (Best score so-far: %.4f \u00B1 '
+                      '%.4f) (%i / %i)' % (datetime.now().strftime('%H:%M'), np.mean(scoring), np.std(scoring),
+                                           np.mean(timing), self.best[0], self.best[1], i + 1, len(self.parsed_params)))
             self.result.append({
                 'scoring': scoring,
                 'mean_objective': np.mean(scoring),
