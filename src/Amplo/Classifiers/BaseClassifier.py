be5e5d05a7ed7b8b7414bd61c306acbde95bf3e7
Niels Uitterdijk <niels.uit@gmail.com>
2021-05-25 13:03:22 +0200

    CatboostClassifier

---
+++ src/Amplo/Classifiers/BaseClassifier.py
@@ -0,0 +1,27 @@
+class BaseClassifier:
+
+    def __init__(self, model, params):
+        self.defaultParams = params
+        self.model = model(**params)
+        self.hasPredictProba = hasattr(model, 'predict_proba')
+
+    def get_params(self):
+        return self.model.get_params()
+
+    def _set_params(self, args):
+        self.model.set_params(**args)
+        return self
+
+    def predict(self, x):
+        return self.model.predict(x)
+
+    def predict_proba(self, x):
+        if self.hasPredictProba:
+            return self.model.predict_proba(x)
+
+    def score(self, x, y):
+        return self.model.score(x, y)
+
+    def fit(self, x, y):
+        self.model.fit(x, y)
+        return self
---
+++ src/Amplo/Classifiers/__init__.py
@@ -0,0 +1,1 @@
+from CatboostClassifier import CatboostClassifier
