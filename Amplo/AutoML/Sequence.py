86c7ffb6f8d710ff31fed355a6bb655da9690160
Niels Uitterdijk <niels.uit@gmail.com>
2021-06-22 16:38:49 +0200

    Sequence launch
    Testing
    all functional

--- Amplo/AutoML/Sequence.py
+++ Amplo/AutoML/Sequence.py
@@ -1,94 +1,167 @@
+import warnings
 import numpy as np
 import pandas as pd
 
 
 class Sequence:
+    # todo implement fractional differencing
 
-    def __init__(self, back=0, forward=0, shift=0, diff='none'):
+    def __init__(self, back=1, forward=1, shift=0, diff='none'):
         """
         Sequencer. Sequences and differentiate data.
-        Scenarios:
-        - Sequenced I/O                     --> back & forward in int or list of ints
-        - Sequenced input, single output    --> back: int, forward: list of ints
-        - Single input, single output       --> back & forward =
+        The indices of back and forward start from 0. Therefore, if the output is included in the input,
+        Having forward = 4 will result in predicting the output for [t, t+1, t+2, t+3].
+        Having forward = [4] will result in making a t+4 prediction.
+        Having back = 30 will result in the all samples [t, t-1, ..., t-29] to be included.
+        Having back = [30] will result in the sample [t-30] to be included.
 
-        :param back: Int or List[int]: input indices to include
-        :param forward: Int or List[int]: output indices to include
+        :param back: Int or List[int]: Input indices.
+        If list -> includes all integers within the list
+        If int -> includes that many samples back
+        :param forward: Int or List[int]: Output indices.
+        If list -> includes all integers within the list.
+        If int -> includes that many samples forward.
         :param shift: Int: If there is a shift between input and output
-        :param diff: differencing algo, pick between 'none', 'diff', 'log_diff', 'fractional' (no revert)
+        :param diff: differencing algo, pick between 'none', 'diff', 'log_diff'
         """
-        if type(back) == int:
-            back = np.linspace(0, back, back+1).astype('int')
-            self.inputDType = 'int'
-        elif type(back) == list:
-            back = np.array(back)
-            self.inputDType = 'list'
+        # Tests
+        assert isinstance(back, int) or all([isinstance(x, int) for x in back]), \
+            'Back needs to be of type int or list[int].'
+        assert isinstance(forward, int) or all([isinstance(x, int) for x in forward]), \
+            'Back needs to be of type int or list[int].'
+        assert isinstance(shift, int), 'Shift needs to be of type int.'
+        assert diff in ['none', 'diff', 'log_diff'], 'Diff needs to be none, diff or log_diff.'
+        if isinstance(back, int):
+            assert back > 0, "'back' arg needs to be strictly positive."
         else:
-            raise ValueError('Back needs to be int or list(int)')
-        if type(forward) == int:
-            self.outputDType = 'int'
-            forward = np.linspace(0, forward, forward+1).astype('int')
-        elif type(forward) == list:
-            self.outputDType = 'list'
-            forward = np.array(forward)
+            assert all([x > 0 for x in back]), "All integers in 'back' need to be strictly positive."
+            assert all([x > 0 for x in np.diff(back)]), "All integers in 'back' need to be monotonically increasing."
+        if isinstance(forward, int):
+            assert forward > 0, "'forward' arg needs to be strictly positive"
         else:
-            raise ValueError('Forward needs to be int or list(int)')
-        self.backVec = back
-        self.foreVec = forward
-        self.nBack = len(back)
-        self.nFore = len(forward)
-        self.foreRoll = np.roll(self.foreVec, 1)
-        self.foreRoll[0] = 0
-        self.mBack = max(back) - 1
-        self.mFore = max(forward) - 1
+            assert all([x > 0 for x in forward]), "All integers in 'forward' need to be strictly positive."
+            assert all([x > 0 for x in np.diff(forward)]), \
+                "All integers in 'forward' need to be monotonically increasing."
+        if diff != 'none' and isinstance(back, int):
+            assert back > 1, "With differencing, back needs to be at least 2."
+        if diff != 'none' and isinstance(forward, int):
+            assert forward > 1, "With differencing, forward needs to be at least 2."
+
+        # Note static args
         self.shift = shift
         self.diff = diff
-        self.samples = 1
-        if diff != 'none':
-            self.samples = 0
-            self.nBack -= 1
-        if diff not in ['none', 'diff', 'log_diff']:
-            raise ValueError('Type should be in [None, diff, log_diff, fractional]')
+        self.samples = 0            # Add 1 as in/out-put start both at 0
+        self.inputConstant = 0
+        self.outputConstant = 0
 
-    def convert(self, x, y):
+        # Parse args
+        if type(back) == int:
+            back = np.linspace(0, back - 1, back).astype('int')
+        elif type(back) == list:
+            back = np.array(back)
+        if type(forward) == int:
+            forward = np.linspace(0, forward - 1, forward).astype('int')
+        elif type(forward) == list:
+            forward = np.array(forward)
+
+        # Index Vectors
+        self.inputIndices = back
+        self.inputDiffIndices = None
+        self.outputIndices = forward
+        self.outputDiffIndices = None
+
+        # Differencing vectors
+        if diff != 'none':
+            # In case first is 0, we won't difference with -1, therefore, we add & skip the first
+            if self.inputIndices[0] == 0:
+                self.inputDiffIndices = self.inputIndices[:-1]
+                self.inputIndices = self.inputIndices[1:]
+            else:
+                # However, if first is nonzero, we can keep all and roll them, changing first one to 0
+                self.inputDiffIndices = np.roll(self.inputIndices, 1)
+                self.inputDiffIndices[0] = 0
+
+            # Same for output
+            if self.outputIndices[0] == 0:
+                self.outputDiffIndices = self.outputIndices[:-1]
+                self.outputIndices = self.outputIndices[1:]
+            else:
+                self.outputDiffIndices = np.roll(self.outputIndices, 1)
+                self.outputDiffIndices[0] = 0
+
+        # Number of sequence steps
+        self.nInputSteps = len(self.inputIndices)
+        self.nOutputSteps = len(self.outputIndices)
+
+        # Maximum steps
+        self.maxInputStep = max(self.inputIndices)
+        self.maxOutputStep = max(self.outputIndices)
+
+    def convert(self, x, y, flat=True):
+        """
+        Sequences input / outputs dataframe / numpy array.
+
+        :param x: Input
+        :param y: Output
+        :param flat: Boolean. If true, a flat matrix is returned. If false, a 3D tensor is returned (numpy).
+        :return: seq_x, seq_y
+        """
         if isinstance(x, pd.DataFrame) or isinstance(x, pd.core.series.Series):
             assert isinstance(y, pd.DataFrame) or isinstance(y, pd.core.series.Series), \
                 'Input and Output need to be the same data type.'
-            return self.convert_pandas(x, y)
+            return self._convert_pandas(x, y, flat=flat)
         elif isinstance(x, np.ndarray):
             assert isinstance(y, np.ndarray), 'Input and Output need to be the same data type.'
-            return self.convert_numpy(x, y)
+            return self._convert_numpy(x, y, flat=flat)
         else:
             TypeError('Input & Output need to be same datatype, either Numpy or Pandas.')
 
-    def convert_numpy(self, x, y):
-        # todo implement flat
+    def _convert_numpy(self, x, y, flat=True):
+        # Initializations
         if x.ndim == 1:
             x = x.reshape((-1, 1))
         if y.ndim == 1:
             y = y.reshape((-1, 1))
-        samples = len(x) - self.mBack - self.mFore - self.shift - 1
+        # Samples, interval minus sequence length (maxIn+maxOutPlus+shift)
+        self.samples += len(x) - self.maxInputStep - self.maxOutputStep - self.shift
         features = len(x[0])
-        input_sequence = np.zeros((samples, self.nBack, features))
-        output_sequence = np.zeros((samples, self.nFore))
+        input_sequence = np.zeros((self.samples, self.nInputSteps, features))
+        output_sequence = np.zeros((self.samples, self.nOutputSteps))
+
+        # Sequence
         if self.diff == 'none':
-            for i in range(samples):
-                input_sequence[i] = x[i + self.backVec]
-                output_sequence[i] = y[i - 1 + self.mBack + self.shift + self.foreVec].reshape((-1))
-            return input_sequence, output_sequence
+            for i in range(self.samples):
+                input_sequence[i] = x[i + self.inputIndices]
+                output_sequence[i] = y[i + self.maxInputStep + self.shift + self.outputIndices].reshape((-1))
+
         elif self.diff[-4:] == 'diff':
+
+            # Take log for log_diff
             if self.diff == 'log_diff':
-                x = np.log(x)
-                y = np.log(y)
-            if (self.backVec == 0).all():
-                self.backVec = np.array([0, 1])
-            for i in range(samples):
-                input_sequence[i] = x[i + self.backVec[1:]] - x[i + self.backVec[:-1]]
-                output_sequence[i] = (y[i + self.mBack + self.shift + self.foreVec] -
-                                      y[i + self.mBack + self.shift + self.foreRoll]).reshape((-1))
+                if np.min(x) < 1e-3:
+                    self.inputConstant = abs(np.min(x) * 1.001) + 1e-3
+                    warnings.warn('Small or negative input values found, adding a constant {:.2e} to input'.format(
+                        self.inputConstant))
+                if np.min(y) < 1e-3:
+                    self.outputConstant = abs(np.min(y) * 1.001) + 1e-3
+                    warnings.warn('Small or negative output values found, adding a constant {:.2e} to output'.format(
+                        self.outputConstant))
+                x = np.log(x + self.inputConstant)
+                y = np.log(y + self.outputConstant)
+
+            # Finally create the difference vector
+            for i in range(self.samples):
+                input_sequence[i] = x[i + self.inputIndices] - x[i + self.inputDiffIndices]
+                output_sequence[i] = (y[i + self.maxInputStep + self.shift + self.outputIndices] -
+                                      y[i + self.maxInputStep + self.shift + self.outputDiffIndices]).reshape((-1))
+
+        # Return
+        if flat:
+            return input_sequence.reshape((self.samples, self.nInputSteps * features)), output_sequence
+        else:
             return input_sequence, output_sequence
 
-    def convert_pandas(self, x, y):
+    def _convert_pandas(self, x, y, flat=True):
         # Check inputs
         if isinstance(x, pd.core.series.Series):
             x = x.to_frame()
@@ -98,59 +171,90 @@ class Sequence:
         assert isinstance(x, pd.DataFrame)
         assert isinstance(y, pd.DataFrame)
 
-        # Keys
+        # Initials
         input_keys = x.keys()
         output_keys = y.keys()
+        lag = None              # Input indices
+        shift = None            # Output indices
 
-        # No Differencing
-        if self.diff == 'none':
-            # Input
-            for lag in self.backVec:
-                keys = [key + '_' + str(lag) for key in input_keys]
-                x[keys] = x[input_keys].shift(lag)
+        # If flat return
+        if flat:
+            # No Differencing
+            if self.diff == 'none':
+                # Input
+                for lag in self.inputIndices:
+                    keys = [key + '_' + str(lag) for key in input_keys]
+                    x[keys] = x[input_keys].shift(lag)
 
-            # Output
-            for shift in self.foreVec:
-                keys = [key + '_' + str(shift) for key in output_keys]
-                y[keys] = y[keys].shift(-shift)
+                # Output
+                for shift in self.outputIndices:
+                    keys = [key + '_' + str(shift) for key in output_keys]
+                    y[keys] = y[output_keys].shift(-shift)
 
-        # With differencing
-        elif self.diff[-4:] == 'diff':
-            # Input
-            for lag in self.backVec:
-                # Shifted
-                keys = [key + '_' + str(lag) for key in input_keys]
-                x[keys] = x[input_keys].shift(lag)
+            # With differencing
+            elif self.diff[-4:] == 'diff':
+                # If log_diff
+                if self.diff == 'log_diff':
+                    x = np.log(x)
+                    y = np.log(y)
+                # Input
+                for lag in self.inputIndices:
+                    # Shifted
+                    keys = [key + '_' + str(lag) for key in input_keys]
+                    x[keys] = x[input_keys].shift(lag)
 
-                # differentiated
-                d_keys = [key + '_d_' + str(lag) for key in input_keys]
-                x[d_keys] = x[input_keys].shift(lag) - x[input_keys]
+                    # differentiated
+                    d_keys = [key + '_d_' + str(lag) for key in input_keys]
+                    x[d_keys] = x[input_keys].shift(lag) - x[input_keys]
 
-            # Output
-            for shift in self.foreVec:
-                # Only differentiated
-                keys = [key + '_' + str(shift) for key in output_keys]
-                y[keys] = y[output_keys].shift(shift) - y[output_keys]
+                # Output
+                for shift in self.outputIndices:
+                    # Only differentiated
+                    keys = [key + '_' + str(shift) for key in output_keys]
+                    y[keys] = y[output_keys].shift(shift) - y[output_keys]
 
-        # Drop _0 (same as original)
-        x = x.drop([key for key in x.keys() if '_0' in key], axis=1)
-        y = y.drop([key for key in y.keys() if '_0' in key], axis=1)
+            # Drop _0 (same as original)
+            x = x.drop([key for key in x.keys() if '_0' in key], axis=1)
+            y = y.drop([key for key in y.keys() if '_0' in key], axis=1)
 
-        # Return (first lags are NaN, last shifts are NaN
-        return x.iloc[lag:-shift if shift > 0 else None], y.iloc[lag:-shift if shift > 0 else None]
+            # Return (first lags are NaN, last shifts are NaN
+            return x.iloc[lag:-shift if shift > 0 else None], y.iloc[lag:-shift if shift > 0 else None]
+        else:
+            x, y = x.to_numpy(), y.to_numpy()
+            return self._convert_numpy(x, y, flat=False)
 
-    def revert(self, differentiated, original):
-        # Revert the sequencing loop: d = np.h stack((d[0], d[0] + np.cum sum(dd)))
-        if self.nFore == 1:
-            differentiated = differentiated.reshape((-1, 1))
-        y = np.zeros_like(differentiated)
-        if self.diff == 'none':
-            return
-        if self.diff == 'log_diff':
-            for i in range(self.nFore):
-                y[:, i] = np.log(original[self.mBack + self.foreRoll[i]:-self.foreVec[i]]) + differentiated[:, i]
-            return np.exp(y)
+    def revert(self, seq_y, y_start):
+        """
+        Reverts the sequenced vector back. Useful for sequenced predictions in production.
+        :param seq_y: The sequenced / predicted signal,
+        :param y_start: Starting vector, always necessary when differentiated.
+        :return: y: normal predicted signal, de-sequenced.
+        """
+        print('Start Y: {}'.format(y_start))
+        assert len(seq_y.shape) == 2 and seq_y.shape[1] == self.nOutputSteps, "revert() only suitable for output."
+        # assert len(y_start) == self.maxOutputStep + self.shift + 1
+
+        # Initiate
+        y = np.zeros((self.nOutputSteps, len(seq_y) + len(y_start)))
+
+        # de-sequence if diff
         if self.diff == 'diff':
-            for i in range(self.nFore):
-                y[:, i] = original[self.mBack + self.foreRoll[i]:-self.foreVec[i]] + differentiated[:, i]
+            for i in range(self.nOutputSteps):
+                y[i, :len(y_start)] = y_start
+                for j in range(len(seq_y)):
+                    y[i, j + self.outputIndices[i]] = y[0, j + self.outputDiffIndices[i]] + seq_y[j, i]
             return y
+
+        # de-sequence if log_diff (take log of start, add sequence log, then take exp (as x=exp(log(x)))
+        elif self.diff == 'log_diff':
+            for i in range(self.nOutputSteps):
+                y[i, :len(y_start)] = np.log(y_start + self.outputConstant)
+                for j in range(len(seq_y)):
+                    y[i, j + self.outputIndices] = y[0, j + self.outputDiffIndices] + seq_y[j, i]
+                return np.exp(y) - self.outputConstant
+
+        else:
+            if self.diff == 'none':
+                raise ValueError('With differencing set to none, reverting is not necessary.')
+            else:
+                raise ValueError('Differencing method not implemented.')
--- tests/Unit/AutoML/test_sequence.py
+++ tests/Unit/AutoML/test_sequence.py
@@ -1,1 +1,160 @@
-# todo test class
+import unittest
+import numpy as np
+import pandas as pd
+from Amplo.AutoML import Sequence
+
+
+class TestSequence(unittest.TestCase):
+
+    def test_init(self):
+        assert Sequence(), 'Class initiation failed'
+
+    def test_numpy_none(self):
+        # Parameters
+        features = 5
+        length = 500
+        back = np.random.randint(1, 50)
+        x, y = np.random.randint(0, 50, (length, features)), np.random.randint(0, 50, length)
+
+        # Iterate scenarios
+        for forward in [[1], [5]]:
+            # Sequence
+            sequence = Sequence(back=back, forward=forward)
+            seq_x, seq_y = sequence.convert(x, y, flat=False)
+
+            # Test
+            assert len(seq_x.shape) == 3, 'seq_x is no tensor'
+            assert seq_x.shape[1] == back, 'seq_x step dimension incorrect'
+            assert seq_x.shape[2] == features, 'seq_x feature dimension incorrect'
+            assert seq_x.shape[0] == seq_y.shape[0], 'seq_x and seq_y have inconsistent samples'
+            for i in range(seq_x.shape[0]):
+                assert np.allclose(seq_x[i], x[i:i + back]), 'seq_x samples are not correct'
+                assert np.allclose(seq_y[i], y[i + back + forward[0] - 1]), 'seq_y samples are not correct'
+
+    def test_numpy_diff(self):
+        # Parameters
+        features = 5
+        length = 500
+        back = np.random.randint(2, 50)
+        x = np.outer(np.linspace(1, length, length), np.ones(features))
+        y = np.linspace(0, length - 1, length)
+
+        # Iterate Scenarios
+        for forward in [[1], [5]]:
+            # Sequence
+            sequence = Sequence(back=back, forward=forward, diff='diff')
+            seq_x, seq_y = sequence.convert(x, y, flat=False)
+
+            # Tests
+            assert len(seq_x.shape) == 3, 'seq_x is not a tensor'
+            assert seq_x.shape[1] == back - 1, 'seq_x step dimensions incorrect'
+            assert seq_x.shape[2] == features, 'seq_x feature dimensions incorrect'
+            assert seq_x.shape[0] == seq_y.shape[0], 'seq_x and seq_y inconsistent samples'
+            assert np.allclose(seq_x, np.ones_like(seq_x)), 'samples seq_x incorrect'
+            assert np.allclose(seq_y, np.ones_like(seq_y) * forward[0]), 'seq_y samples incorrect'
+
+    def test_numpy_log(self):
+        # Parameters
+        features = 5
+        length = 500
+        back = np.random.randint(2, 50)
+        x = np.outer(np.linspace(1, length, length), np.ones(features))
+        y = np.linspace(1, length, length)
+
+        # Iterate Scenarios
+        for forward in [[1], [5]]:
+            # Sequence
+            sequence = Sequence(back=back, forward=forward, diff='log_diff')
+            seq_x, seq_y = sequence.convert(x, y, flat=False)
+
+            # Tests
+            assert len(seq_x.shape) == 3, 'seq_x is not a tensor'
+            assert seq_x.shape[1] == back - 1, 'seq_x step dimensions incorrect'
+            assert seq_x.shape[2] == features, 'seq_x feature dimensions incorrect'
+            assert seq_x.shape[0] == seq_y.shape[0], 'seq_x and seq_y inconsistent samples'
+            for i in range(seq_x.shape[0]):
+                assert np.allclose(seq_x[i], np.log(x[1+i:i+back]) - np.log(x[i:i+back-1])), 'samples seq_x incorrect'
+                assert np.allclose(seq_y[i], np.log(y[i+back+forward[0]-1]) - np.log(y[i+back-1])), \
+                    'seq_y samples incorrect'
+
+    def test_numpy_multi_out(self):
+        # Parameters
+        features = 5
+        length = 500
+        forward = [np.random.randint(2, 10)]
+        forward += [forward[0] + np.random.randint(1, 10)]
+        back = np.random.randint(2, 50)
+        x = np.outer(np.linspace(1, length, length), np.ones(features))
+        y = np.linspace(1, length, length)
+
+        # Without differencing
+        sequence = Sequence(back=back, forward=forward, diff='none')
+        seq_x, seq_y = sequence.convert(x, y, flat=False)
+        # Test
+        assert seq_x.shape[0] == seq_y.shape[0], 'seq_x and seq_y have inconsistent samples'
+        assert seq_y.shape[1] == 2, 'seq_y has incorrect steps'
+        for i in range(seq_x.shape[0]):
+            assert np.allclose(seq_y[i].tolist(),
+                               [y[i + back + forward[0] - 1], y[i + back + forward[1] - 1]]
+                               ), 'seq_y samples are not correct'
+
+        # With differencing
+        sequence = Sequence(back=back, forward=forward, diff='diff')
+        seq_x, seq_y = sequence.convert(x, y, flat=False)
+        revert = sequence.revert(seq_y, y[back - 1:back - 1 + max(forward)])
+        # Tests
+        assert seq_x.shape[0] == seq_y.shape[0], 'seq_x and seq_y have inconsistent samples'
+        assert seq_y.shape[1] == 2, 'seq_y has incorrect steps'
+        assert np.allclose(revert[0, :forward[0] - forward[-1]], y[back - 1: forward[0] - forward[1]])
+        assert np.allclose(revert[1], y[back - 1:])
+
+
+    def test_pandas_tensor(self):
+        # Parameters
+        features = 5
+        length = 500
+        back = np.random.randint(1, 50)
+        x, y = np.random.randint(0, 50, (length, features)), np.random.randint(0, 50, length)
+        x, y = pd.DataFrame(x), pd.Series(y)
+
+        # Iterate scenarios
+        for forward in [[1], [5]]:
+            # Sequence
+            sequence = Sequence(back=back, forward=forward)
+            seq_x, seq_y = sequence.convert(x, y, flat=False)
+
+            # Tests
+            assert len(seq_x.shape) == 3, 'seq_x is no tensor'
+            assert seq_x.shape[1] == back, 'seq_x step dimension incorrect'
+            assert seq_x.shape[2] == features, 'seq_x feature dimension incorrect'
+            assert seq_x.shape[0] == seq_y.shape[0], 'seq_x and seq_y have inconsistent samples'
+            for i in range(seq_x.shape[0]):
+                assert np.allclose(seq_x[i], x[i:i + back]), 'seq_x samples incorrect'
+                assert np.allclose(seq_y[i], y[i + back + forward[0] - 1]), 'seq_y samples incorrect'
+
+    def test_reconstruction(self):
+        # Parameters
+        length = 100
+        features = 5
+        back = np.random.randint(2, 10)
+        forward = [np.random.randint(2, 10)]
+        # x, y = np.random.randint(0, 50, (length, features)), np.random.randint(0, 50, length)
+        x, y = np.outer(np.linspace(1, length, length), np.ones(features)), np.linspace(1, length, length)
+
+        # Iterate scenarios
+        for diff in ['diff', 'log_diff']:
+            # Sequence
+            seq = Sequence(back=back, forward=forward, diff=diff)
+            seq_x, seq_y = seq.convert(x, y, flat=False)
+
+            # Tests
+            assert len(seq_x.shape) == 3, 'seq_x is not a tensor'
+            assert seq_x.shape[1] == back - 1, 'seq_x step dimensions incorrect'
+            assert seq_x.shape[2] == features, 'seq_x feature dimensions incorrect'
+            assert seq_x.shape[0] == seq_y.shape[0], 'seq_x and seq_y inconsistent samples'
+            revert = seq.revert(seq_y, y[back - 1:back - 1 + forward[0]])
+            assert np.allclose(revert, y[back - 1:]), 'reverted seq_y incorrect'
+
+
+if __name__ == '__main__':
+    TestSequence()
